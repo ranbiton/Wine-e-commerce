bdb01c1cf57576b8cb67341a90c08298e615b9e7
ranbiton <125357583+ranbiton@users.noreply.github.com>
2024-08-01 12:35:50 +0300

    Add files via upload

---
+++ client frontend/src/components/homeComponents/CalltoActionSection.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const CalltoActionSection = () => {
+  return (
+    <div className="subscribe-section bg-with-black">
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12">
+            <div className="-subscribehead">
+             
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CalltoActionSection;
